3fc9c6575139b1273dc201b9d107fa36e502b893
Ex0t1g <140333383+Ex0t1g@users.noreply.github.com>
2024-01-25 17:16:22 +0300

    Update Project11.cpp

--- Project11.cpp
+++ Project11.cpp
@@ -1,29 +1,78 @@
 #include <iostream>
-#include <vector>
-#include <ctime>
-
+#include <list>
 using namespace std;
-int main() {
-    setlocale(LC_ALL, "Ru");
-    int size;
-    cout << "Введите размер вектора: ";
-    cin >> size;
 
-    vector<int> myVector(size);
-    srand(time(0));
+class Weapon
+{
+protected:
+    int damage;
+    int shootCount;
+    int maxAmmo;
+public:
 
-    cout << "Заполнение вектора случайными числами:" << endl;
-    for (int i = 0; i < size; i++) {
-        myVector[i] = rand();
-        cout << myVector[i] << " ";
+    Weapon(int damage, int shootCount, int maxAmmo) : damage(damage), shootCount(shootCount), maxAmmo(maxAmmo) {}
+    virtual int shoot() = 0;
+};
+
+class Blaster : public Weapon {
+public:
+    Blaster() : Weapon(10, -1, -1) {}
+    int shoot() {
+        return damage;
     }
-    cout << endl;
+};
 
-    cout << "Вектор в обратном порядке:" << endl;
-    for (int i = size - 1; i >= 0; i--) {
-        cout << myVector[i] << " ";
+class Pistol : public Weapon {
+public:
+    Pistol() : Weapon(20, 6, 6) {}
+    int shoot() {
+        if (shootCount > 0) {
+            shootCount--;
+            return damage;
+        }
+        return 0;
     }
-    cout << endl;
+};
+
+class MachineGun : public Weapon {
+public:
+    MachineGun() : Weapon(5, 100, 100) {}
+    int shoot() {
+        if (shootCount > 0) {
+            shootCount--;
+            return damage;
+        }
+        return 0;
+    }
+};
+
+int main()
+{
+    list<Weapon*> weapons;
+    Blaster* blaster = new Blaster();
+    Pistol* pistol = new Pistol();
+    MachineGun* machineGun = new MachineGun();
+
+    weapons.push_back(blaster);
+    weapons.push_back(pistol);
+    weapons.push_back(machineGun);
+
+    while (weapons.size() > 1) {
+        Weapon* currentWeapon = weapons.front();
+        cout << "Damage: " << currentWeapon->shoot() << endl;
+
+        if (currentWeapon->shootCount == 0) {
+            delete currentWeapon;
+            weapons.pop_front();
+        }
+
+        weapons.push_back(currentWeapon);
+        weapons.pop_front();
+    }
+
+    for (Weapon* weapon : weapons) {
+        delete weapon;
+    }
 
     return 0;
 }
