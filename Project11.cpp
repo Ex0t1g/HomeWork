c0da819ae807b8e5497b5fb73848c59f1ecb2d72
Ex0t1g <140333383+Ex0t1g@users.noreply.github.com>
2024-02-01 10:37:37 +0300

    Update Project11.cpp

--- Project11.cpp
+++ Project11.cpp
@@ -1,78 +1,10 @@
+#include <cstdio>
 
-
-#include <iostream>
-#include <list>
-using namespace std;
-
-class Weapon
-{
-protected:
-    int damage;
-    int shootCount;
-    int maxAmmo;
-public:
-
-    Weapon(int damage, int shootCount, int maxAmmo) : damage(damage), shootCount(shootCount), maxAmmo(maxAmmo) {}
-    virtual int shoot()
-    {
-        if (shootCount > 0) {
-            shootCount--;
-            return damage;
-        }
-        return 0;
-    }
-};
-
-class Blaster : public Weapon {
-public:
-    Blaster() : Weapon(10, INT_MAX ,INT_MAX) {} // Бесконечные патроны
-    int shoot() override
-    {
-        return damage;
-    }
-};
-
-class Pistol : public Weapon {
-public:
-    Pistol() : Weapon(20, 6, 6) {}
-};
-
-class MachineGun : public Weapon {
-public:
-    MachineGun() : Weapon(5, 100, 100) {}
-};
-
-int main()
-{
-    list<Weapon*> weapons;
-    Blaster* blaster = new Blaster();
-    Pistol* pistol = new Pistol();
-    MachineGun* machineGun = new MachineGun();
-
-    weapons.push_back(blaster);
-    weapons.push_back(pistol);
-    weapons.push_back(machineGun);
-
-    int currentEnemyHP = 1000;
-    while (true) {
-        for (Weapon* weapon : weapons) {
-            int damage = weapon->shoot();
-            currentEnemyHP -= damage;
-            cout << "Damage: " << damage << " Current Enemy HP: " << currentEnemyHP << endl;
-            if (currentEnemyHP <= 0) {
-                cout << "Enemy killed!" << endl;
-                break;
-            }
-        }
-
-        if (currentEnemyHP <= 0) {
-            break;
-        }
-    }
-
-    for (Weapon* weapon : weapons) {
-        delete weapon;
-    }
+int main() {
+    FILE* fp = fopen("my.txt", "w");
+    const char* phrase = "Mama myla ramu";
+    int written = fwrite(phrase, sizeof(char), 13, fp);
+    fclose(fp);
 
     return 0;
 }
